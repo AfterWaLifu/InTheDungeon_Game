26361012dcab98c2702f379e453d4ad6dc09fa8a
AfterWaLifu <maximaphonin@gmail.com>
2021-06-01 16:47:28 +0300

    added map editor project

---
+++ ITD_Map_Editor/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace ITD_Map_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(656, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(58, 22);
+            this.toolStripButton1.Text = "Открыть";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(70, 22);
+            this.toolStripButton2.Text = "Сохранить";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(78, 22);
+            this.toolStripButton3.Text = "Новая карта";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(220, 420);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "InTheDungeon Map Editor";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ ITD_Map_Editor/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ITD_Map_Editor
+{
+    public partial class Form1 : Form
+    {
+        char[,] mapInText;
+        PictureBox[,] mapInImages;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.ToString())
+            {
+                case "Открыть":
+                    break;
+                case "Сохранить":
+                    break;
+                case "Новая карта":
+                    NewMap();
+                    break;
+            }
+        }
+
+        void OpenMap()
+        {
+            
+        }
+
+        void SaveMap()
+        {
+
+        }
+
+        void NewMap()
+        {
+            string[] temp = new string[3] {"111","101","111" };
+            richTextBox1.Lines = temp;
+            DrawMap();
+        }
+
+        void DrawMap()
+        {
+            textMapFiller();
+            boxesCreator(mapInText.GetLength(0), mapInText.GetLength(1));
+
+            for (int x = 0; x < mapInText.GetLength(0); x++)
+            {
+                for (int y = 0; y < mapInText.GetLength(1); y++)
+                {
+                    switch (mapInText[x, y])
+                    {
+                        case '0':
+                            mapInImages[x,y].Image = Properties.Resources._0;
+                            break;
+                        case '1':
+                            mapInImages[x,y].Image = Properties.Resources._1;
+                            break;
+                        case '2':
+                            mapInImages[x,y].Image = Properties.Resources._2;
+                            break;
+                        case '3':
+                            mapInImages[x,y].Image = Properties.Resources._3;
+                            break;
+                        case '4':
+                            mapInImages[x,y].Image = Properties.Resources._4;
+                            break;
+                        case '5':
+                            mapInImages[x,y].Image = Properties.Resources._5;
+                            break;
+                        case '6':
+                            mapInImages[x,y].Image = Properties.Resources._6;
+                            break;
+                        case '7':
+                            mapInImages[x,y].Image = Properties.Resources._7;
+                            break;
+                        case '8':
+                            mapInImages[x,y].Image = Properties.Resources._8;
+                            break;
+                        default:
+                            mapInImages[x,y].Image = Properties.Resources._2;
+                            break;
+                    }
+                }
+            }
+        }
+
+        void textMapFiller()
+        {
+            string[] lines= richTextBox1.Lines;
+
+            int maxI, maxVal = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].Length > maxVal)
+                {
+                    maxI = i;
+                    maxVal = lines[i].Length;
+                }
+            }
+
+            mapInText = new char[maxVal, lines.Length];
+
+            for (int x = 0; x < maxVal; x++)
+            {
+                for (int y = 0; y < lines.Length; y++)
+                {
+                    mapInText[x, y] = lines[x][y];
+                }
+            }
+        }
+
+        void boxesCreator(int x, int y)
+        {
+            mapInImages = new PictureBox[mapInText.GetLength(0), mapInText.GetLength(1)];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    PictureBox temp = new PictureBox();
+                    temp.Size = new Size(20, 20);
+                    temp.Location = new Point(220 + i * 20, 28 + j * 20);
+                    temp.SizeMode = PictureBoxSizeMode.StretchImage;
+                    mapInImages[i, j] = temp;
+                    this.Controls.Add(mapInImages[i, j]);
+                }
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            DrawMap();
+        }
+    }
+}
